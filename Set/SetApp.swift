7db719710d0c827f8d47435367e939653bf102de
Brice <BriceBro@usc.edu>
2022-12-30 21:40:43 +0100

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Brice Brown on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
